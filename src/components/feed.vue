b8e80c1ae0af54ae4ab3e0156891d0216999f961
Faruq2040 <farusmith5@gmail.com>
2020-11-05 22:18:47 +0300

    designed blog cards

--- src/components/feed.vue
+++ src/components/feed.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <button v-if="!postArea" @click="postArea = true" class="w-100 p-2 mb-2 btn btn-primary">Wanna Post </button>
+        <button v-if="!postArea" @click="postArea = true" class="w-100 p-2 mb-2 btn btn-warning">Post What's Happening Now </button>
         <form v-else class="form-group">
             <textarea class="form-control mb-3" v-model="title" rows="1" id="my_editor" placeholder="title"></textarea>
             <textarea class="form-control" v-model="content" rows="5" id="my_editor" placeholder="what is on your mind?"></textarea>
--- src/components/showBlog.vue
+++ src/components/showBlog.vue
@@ -2,11 +2,11 @@
   <div> 
       <div class="row mb-2">
             <div class="col-md-6" v-for="post in posts.reverse()" :key="post.id">
-                <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-primary">World</strong>
+                <div class="row no-gutters shadow-lg border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div style="font-family:Verdana;" class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-white bg-primary">World</strong>
                         <h3 class="mb-0">{{ post.title }} </h3>
-                        <div class="mb-1 text-muted">Nov 12</div>
+                        <div class="mb-1 mr-auto w-25 bg-warning text-transparent">Nov 12</div>
                         <p class="card-text mb-auto">{{ post.content }}</p>
                         <a href="#" class="stretched-link">Continue reading</a>
                     </div>
