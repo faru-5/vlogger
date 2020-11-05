094625d5eec4b15d2b4a3e873492e1bbcf1dab1d
Faruq2040 <farusmith5@gmail.com>
2020-11-05 18:30:31 +0300

    starting the app

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog'
+
+export default {
+  components:{
+    'add-blog':addBlog
+  }
+
+}
+</script>
+
+<style>
+
+</style>
